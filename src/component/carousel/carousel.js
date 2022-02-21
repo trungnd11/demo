98e361c9f74f160eb30d00a2b91416a8f55669c9
DESKTOP-IVN453T\Admin <trung.dn9500@gmail.com>
2022-02-21 16:46:36 +0700

    First Commit Update code

---
+++ src/component/carousel/carousel.js
@@ -0,0 +1,16 @@
+import Carousel from "nuka-carousel";
+
+function SlideCarousel() {
+  return (
+    <Carousel>
+      <img src="https://picsum.photos/800/300/?random" />
+      <img src="https://picsum.photos/800/300/?random" />
+      <img src="https://picsum.photos/800/300/?random" />
+      <img src="https://picsum.photos/800/300/?random" />
+      <img src="https://picsum.photos/800/300/?random" />
+      <img src="https://picsum.photos/800/300/?random" />
+    </Carousel>
+  );
+}
+
+export default SlideCarousel;
